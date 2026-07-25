4801178fb270d002cf59c6e81fd7b761ea311a44
Eduardo Lustosa <eduardolusribeiro@gmail.com>
2026-07-25 16:03:56 -0300

    Inicialização do Sistema
    - Adiciona e configura driver mpu6050.py para comunicação I2C não-bloqueante.
    
    - Declara variáveis de controle e pinagem do sensor e botão.
    
    - Implementa a mensagem de inicialização no serial ("Sistema de Monitoramento Inicializado").

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Sistema Kanban Inicializado")
+from machine import Pin, I2C
+import mpu6050
+import time
+
+i2c = I2C(0, scl=Pin(21), sda=Pin(22))
+sensor_de_temperatura = mpu6050.accel(i2c)
+botao = Pin(27, Pin.IN, Pin.PULL_UP)
+
+print("Sistema de Monitoramento Inicializado")
+
+while True:
+  pass
